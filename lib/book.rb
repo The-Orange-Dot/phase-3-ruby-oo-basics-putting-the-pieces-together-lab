8b01c96bd73cc378aefd023ba32d83e6866ecae7
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-10 19:11:51 +0900

    Completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+  attr_reader :title
+  attr_accessor :page_count, :author, :genre
 
+  def initialize(book)
+    @title = book
+  end
 
+  def turn_page
+    puts 'Flipping the page...wow, you read fast!'
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+  attr_reader :brand
+  attr_accessor :condition, :color, :size, :material
+
+  def initialize(shoe)
+    @brand = shoe
+
+    def cobble
+      puts 'Your shoe is as good as new!'
+      puts @condition = 'new'
+    end
+  end
+end
